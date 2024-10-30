47d14f498d841654dc3694d462db5992351c9e1b
Layton <jihwan8807@naver.com>
2024-10-30 13:49:53 +0900

    implement parser

---
+++ src/parser.js
@@ -0,0 +1,40 @@
+// 토큰 사이의 관계 찾기
+export function parser(tokens) {
+  // AST는 추상 구문 트리
+  const AST = {
+    type: 'Drawing',
+    body: [],
+  };
+
+  while (tokens.length > 0) {
+    let currentToken = tokens.shift();
+    const { type, value } = currentToken;
+    if (type === 'word') {
+      switch (value) {
+        case 'Paper':
+          const expression = {
+            type: 'CallExpression',
+            name: 'Paper',
+            arguments: [],
+          };
+          const argument = tokens.shift();
+          const { type, value } = argument;
+          if (type === 'number') {
+            expression.arguments.push({
+              type: 'NumberLiteral',
+              value,
+            });
+            AST.body.push(expression);
+          } else {
+            throw 'Paper command must be followed by a number.';
+          }
+          break;
+        //  case 'Pen' :
+        //   ...
+        // case 'Line':
+        //  ...
+      }
+    }
+  }
+  return AST;
+}
